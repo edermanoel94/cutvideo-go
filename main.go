53982a19b264f42c6a83cddd7d3db33b32ba19c1
edermanoel94 <edercosta94@live.com>
2025-12-26 16:42:10 -0300

    Update

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -24,25 +21,23 @@ type Video struct {
 	Clips          []Clip `json:"clips"`
 }
 
-var (
-	ffmpegLogger, _ = os.Create("./ffmpeg.log")
-	logger          = log.New(ffmpegLogger, "", 0)
-)
+var logger = log.New(os.Stderr, "LOG: ", 0)
 
 func main() {
-	data, err := os.ReadFile("./data.json")
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <input_file>\n", os.Args[0])
+		return
+	}
+
+	data, err := os.ReadFile(os.Args[1])
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
 	var videos []Video
 
 	if err := json.Unmarshal(data, &videos); err != nil {
-		log.Fatal(err)
-	}
-
-	if len(videos) == 0 {
-		log.Fatal(errors.New("no video to cut"))
+		logger.Fatal(err)
 	}
 
 	ctx := context.Background()
@@ -53,7 +48,7 @@ func main() {
 			// outputVideoFile := fmt.Sprintf("%s_%s.mp4", strings.ToLower(video.Title), clipName)
 
 			if err := execFFMPEG(ctx, video.InputVideoPath, clip); err != nil {
-				log.Printf("Failed to execute ffmpeg: %s", err.Error())
+				logger.Printf("Failed to execute ffmpeg: %s", err.Error())
 				break
 			}
 		}
@@ -70,32 +65,15 @@ func execFFMPEG(ctx context.Context, inputVideo string, clip Clip) error {
 		"-ss", clip.StartTime, "-to", clip.EndTime,
 		"-c", "copy", outputVideoFile)
 
-	// stdoutPipe, _ := cmd.StdoutPipe()
-	// stderrPipe, _ := cmd.StderrPipe()
-
 	if err := cmd.Start(); err != nil {
-		log.Printf("Failed to start ffmpeg: %s\n", err.Error())
+		logger.Printf("Failed to start ffmpeg: %s\n", err.Error())
 		return err
 	}
 
-	// go streamLinesToLogger(stdoutPipe, logger)
-	// go streamLinesToLogger(stderrPipe, logger)
-
 	if err := cmd.Wait(); err != nil {
-		log.Printf("ffmpeg exited with error: %s\n", err.Error())
+		logger.Printf("ffmpeg exited with error: %s\n", err.Error())
 		return err
 	}
 
 	return nil
-}
-
-func streamLinesToLogger(r io.Reader, logger *log.Logger) {
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		logger.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		logger.Printf("Scanner error: %s\n", err.Error())
-	}
 }
